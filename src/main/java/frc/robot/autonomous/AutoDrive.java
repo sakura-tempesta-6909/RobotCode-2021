a3a43b08518aad7d979b2ca179a91e3891f300e7
Teraterayuki <33191007+Teityan@users.noreply.github.com>
2021-02-23 12:32:19 +0900

    自動制御クラスなどを追加 (#10)
    * AutoNav Hello
    
    * 自動制御用のクラス等を準備
    コードはまだ汚い
    
    * 緊急修正
    
    * Emergency fix (#2)
    
    * delete unused function
    
    * setFeedForwardをgetFeedForwardに
    ArmのメソッドからUtilのメソッドにし、利便性を高めた
    
    * getFeedForwardにしてなかったので修正
    
    * delete ColorCode
    
    * Interstellar and PowerPort (#11)
    
    Co-authored-by: Team-PC-6909 <team_pc@sakura-temepsta.org>
    
    * refactor
    
    Co-authored-by: Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
    Co-authored-by: Team-PC-6909 <79035337+Team-PC-6909@users.noreply.github.com>
    Co-authored-by: Team-PC-6909 <team_pc@sakura-temepsta.org>
    
    * Merge branch 'dev' of https://github.com/sakura-tempesta-6909/RobotCode-2021 into dev
    
    # Conflicts:
    #	src/main/java/frc/robot/Robot.java
    #	src/main/java/frc/robot/autonomous/AutoNav.java
    #	src/main/java/frc/robot/autonomous/GalacticSearch.java
    
    * Merge branch 'dev' of https://github.com/sakura-tempesta-6909/RobotCode-2021 into dev
    
    # Conflicts:
    #	src/main/java/frc/robot/Robot.java
    #	src/main/java/frc/robot/autonomous/AutoNav.java
    #	src/main/java/frc/robot/autonomous/GalacticSearch.java
    
    指摘事項を修正したはず
    
    Co-authored-by: Team-PC-6909 <team_pc@sakura-tempesta.org>
    Co-authored-by: Hirose-Yuto <42714033+Hirose-Yuto@users.noreply.github.com>
    Co-authored-by: Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
    Co-authored-by: Team-PC-6909 <79035337+Team-PC-6909@users.noreply.github.com>
    Co-authored-by: Team-PC-6909 <team_pc@sakura-temepsta.org>

---
+++ src/main/java/frc/robot/autonomous/AutoDrive.java
@@ -0,0 +1,47 @@
+package frc.robot.autonomous;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import frc.robot.subClass.State;
+import frc.robot.subClass.Util;
+
+public class AutoDrive {
+
+    private final WPI_TalonSRX leftMotor, rightMotor;
+
+    public AutoDrive(WPI_TalonSRX leftMotor, WPI_TalonSRX rightMotor){
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+    }
+
+    public void applyState(State state) {
+        if (state.controlMode == State.ControlMode.m_Auto) {
+            switch (state.autoDriveState){
+                case kAutoNavRed:
+                case kAutoNavBlue:
+                case kGalacticSearchRed:
+                case kGalacticSearchBlue:
+                    setPosition(state.driveLeftSetPosition,state.driveRightSetPosition);
+                    break;
+                case kGalacticSearchDoNothing:
+                case kAutoNavDoNothing:
+                    break;
+            }
+        }
+        System.out.println("left_velocity" + leftMotor.getSelectedSensorVelocity());
+        System.out.println("right_velocity" + rightMotor.getSelectedSensorVelocity());
+    }
+
+    private void setPosition(double driveLeftSetPosition,double driveRightSetPosition){
+        leftMotor.set(ControlMode.Position, driveLeftSetPosition);
+        rightMotor.set(ControlMode.Position, driveRightSetPosition);
+    }
+
+    public double getLeftMotorPosition(){
+        return leftMotor.getSelectedSensorPosition();
+    }
+
+    public double getRightMotorPosition(){
+        return rightMotor.getSelectedSensorPosition();
+    }
+}
